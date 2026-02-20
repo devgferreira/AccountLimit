2507ca04b8f32ba43dc3b5ed2ef9ba768e53a0b0
Gabriel <gabrielferreirasouto.com@gmail.com>
2026-02-20 13:27:48 -0300

    feat: ajustando parametros das abstrações: DeleteLimitManagement e UpdateLimitManagement

--- AccountLimit.Domain/Interface/ILimitManagementRepository.cs
+++ AccountLimit.Domain/Interface/ILimitManagementRepository.cs
@@ -11,8 +11,8 @@ namespace AccountLimit.Domain.Interface
     public interface ILimitManagementRepository
     {
         Task CreateLimitManagement(LimitManagementInfo request);
-        Task UpdateLimitManagement(Guid id, LimitManagementInfo request);
-        Task DeleteLimitManagement(Guid id);
+        Task UpdateLimitManagement(LimitManagementInfo request);
+        Task DeleteLimitManagement(string cpf, string agency);
         Task<List<LimitManagementInfo>> SelectLimitManagement(LimitManagementRequest request);
     }
 }
