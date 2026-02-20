8d2fa88fbb60f7e0da04a3bc5a94bb0b7369367c
Gabriel <gabrielferreirasouto.com@gmail.com>
2026-02-20 16:07:13 -0300

    feat: implementando a camada de infra.data

---
+++ AccountLimit.Infra.Data/Mapping/LimitManagement/LimitManagementMapper.cs
@@ -0,0 +1,41 @@
+﻿using AccountLimit.Domain.Commom;
+using AccountLimit.Domain.Entities.LimitManagement;
+using AccountLimit.Domain.Enums;
+using AccountLimit.Infra.Data.Entities.LimitManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountLimit.Infra.Data.Mapping.LimitManagement
+{
+    public static class LimitManagementMapper
+    {
+        public static LimitManagementEntity ToEntity(this LimitManagementInfo request)
+        {
+            return new LimitManagementEntity
+            {
+                Cpf = request.Cpf.Value,
+                Agency = request.Agency.Value,
+                Account = request.Account.Value,
+                PixTransactionLimit = request.PixTransactionLimit.Value
+            };
+
+        }
+        public static Result<LimitManagementInfo> ToDomain(this LimitManagementEntity entity)
+        {
+            if (!Enum.TryParse<Period>(entity.Period, true, out var period))
+                return Result.Failure<LimitManagementInfo>("Period invalid.");
+
+            return LimitManagementInfo.Create(
+                rawCpf: entity.Cpf,
+                rawAgency: entity.Agency,
+                rawAccount: entity.Account,
+                rawLimit: entity.PixTransactionLimit
+            );
+        }
+    }
+
+    
+}
---
+++ AccountLimit.Infra.Data/Repository/LimitManagement/LimitManagementRepository.cs
@@ -0,0 +1,59 @@
+﻿using AccountLimit.Domain.Entities.LimitManagement;
+using AccountLimit.Domain.Entities.LimitManagement.Request;
+using AccountLimit.Domain.Interface;
+using AccountLimit.Infra.Data.Entities.LimitManagement;
+using AccountLimit.Infra.Data.Mapping.LimitManagement;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountLimit.Infra.Data.Repository.LimitManagementRepository
+{
+    public class LimitManagementRepository : ILimitManagementRepository
+    {
+
+        private readonly IDynamoDBContext _context;
+        public LimitManagementRepository(IDynamoDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateLimitManagement(LimitManagementInfo request)
+        {
+
+            await _context.SaveAsync(request.ToEntity());
+        }
+
+        public async Task DeleteLimitManagement(string cpf)
+        {
+            await _context.DeleteAsync<LimitManagementEntity>(cpf);
+        }
+        public async Task<List<LimitManagementInfo>> SelectLimitManagement(LimitManagementRequest request)
+        {
+            var entities = await _context
+                .QueryAsync<LimitManagementEntity>(request.Cpf)
+                .GetRemainingAsync();
+
+            var result = new List<LimitManagementInfo>();
+
+            foreach (var e in entities)
+            {
+                var domainResult = e.ToDomain();
+                if (domainResult.IsFailure)
+                    throw new Exception(domainResult.Error);
+
+                result.Add(domainResult.Value);
+            }
+
+            return result;
+        }
+        public async Task UpdateLimitManagement(LimitManagementInfo request)
+        {
+            await _context.SaveAsync(request.ToEntity());
+        }
+    }
+}
